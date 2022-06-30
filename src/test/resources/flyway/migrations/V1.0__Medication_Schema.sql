5e5c98a7aea94b730c18d13ece41b170be16db13
thisLinda <la.for.lizzi@gmail.com>
2022-06-30 11:13:20 -0400

    schema and data updated

--- src/test/resources/flyway/migrations/V1.0__Medication_Schema.sql
+++ src/test/resources/flyway/migrations/V1.0__Medication_Schema.sql
@@ -7,51 +7,52 @@ DROP TABLE IF EXISTS adverse_reactions;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE adverse_reactions (
-	reaction_id int NOT NULL AUTO_INCREMENT,
-	severity varchar(40) NOT NULL,
+	reaction_pk int NOT NULL AUTO_INCREMENT,
+	severity enum('SEVERE', 'MODERATE', 'MILD') NOT NULL,
 	reaction varchar(40) NOT NULL,
-	PRIMARY KEY (reaction_id)
+	PRIMARY KEY (reaction_pk)
 );
 
 CREATE TABLE medications (
-	med_id int NOT NULL AUTO_INCREMENT,
+	med_pk int NOT NULL AUTO_INCREMENT,
 	brand_name varchar(40) NOT NULL,
 	has_reaction boolean,
-	PRIMARY KEY (med_id)
+	PRIMARY KEY (med_pk)
 );
 
 CREATE TABLE users (
-	user_id int NOT NULL AUTO_INCREMENT,
+	user_pk int NOT NULL AUTO_INCREMENT,
 	pseudo_name varchar(40) NOT NULL,
 	age int NOT NULL,
 	date_of_eval date NOT NULL,
 	date_of_discharge date NOT NULL,
 	med_dx_icd varchar(40) NOT NULL,
 	tx_dx_icd varchar(40) NOT NULL,
-	PRIMARY KEY (user_id)
+	PRIMARY KEY (user_pk)
 );
 
 CREATE TABLE ar_meds (
-	ar_meds_id int NOT NULL AUTO_INCREMENT,
-	reaction_id int NOT NULL,
-	med_id int NOT NULL,
-	PRIMARY KEY (ar_meds_id),
-	FOREIGN KEY (reaction_id) REFERENCES adverse_reactions (reaction_id)
+	ar_meds_pk int NOT NULL AUTO_INCREMENT,
+	reaction_fk int NOT NULL,
+	med_fk int NOT NULL,
+	PRIMARY KEY (ar_meds_pk),
+	FOREIGN KEY (reaction_fk) REFERENCES adverse_reactions (reaction_pk) ON DELETE CASCADE,
+	FOREIGN KEY (med_fk) REFERENCES medications (med_pk) ON DELETE CASCADE
 );
 
 CREATE TABLE user_meds (
-	user_med_id int NOT NULL AUTO_INCREMENT,
-	med_id int NOT NULL,
-	user_id int NOT NULL,
-	PRIMARY KEY (user_med_id),
-	FOREIGN KEY (med_id) REFERENCES medications (med_id),
-	FOREIGN KEY (user_id) REFERENCES users (user_id)
+	user_med_pk int NOT NULL AUTO_INCREMENT,
+	med_fk int NOT NULL,
+	user_fk int NOT NULL,
+	PRIMARY KEY (user_med_pk),
+	FOREIGN KEY (med_fk) REFERENCES medications (med_pk),
+	FOREIGN KEY (user_fk) REFERENCES users (user_pk)
 );
 
 CREATE TABLE generics (
-	generic_id int NOT NULL AUTO_INCREMENT,
-	med_id int NOT NULL,
+	generic_pk int NOT NULL AUTO_INCREMENT,
+	med_fk int NOT NULL,
 	generic_name varchar(40),
-	PRIMARY KEY (generic_id),
-	FOREIGN KEY (med_id) REFERENCES medications (med_id)
+	PRIMARY KEY (generic_pk),
+	FOREIGN KEY (med_fk) REFERENCES medications (med_pk)
 );
--- src/test/resources/flyway/migrations/V1.1__Medication_Data.sql
+++ src/test/resources/flyway/migrations/V1.1__Medication_Data.sql
@@ -1,45 +1,49 @@
---Adverse Reactions
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(400, 'mild', 'xerostomia');
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(401, 'mild', 'dysgeusia');
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(402, 'severe', 'bronchospasm');
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(403, 'moderate', 'dysphagia');
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(404, 'mild', 'gastroesophageal reflux');
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(405, 'moderate', 'esophagitis');
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(406, 'severe', 'tardive dyskinesia');
-INSERT INTO adverse_reactions (reaction_id, severity, reaction) VALUES(407, 'severe', 'laryngospasm');
+--Mild
+INSERT INTO adverse_reactions (severity, reaction) VALUES('mild', 'xerostomia');
+INSERT INTO adverse_reactions (severity, reaction) VALUES('mild', 'dysgeusia');
+INSERT INTO adverse_reactions (severity, reaction) VALUES('mild', 'gastroesophageal reflux');
+
+--Moderate
+INSERT INTO adverse_reactions (severity, reaction) VALUES('moderate', 'dysphagia');
+INSERT INTO adverse_reactions (severity, reaction) VALUES('moderate', 'esophagitis');
+
+--Severe
+INSERT INTO adverse_reactions (severity, reaction) VALUES('severe', 'bronchospasm');
+INSERT INTO adverse_reactions (severity, reaction) VALUES('severe', 'tardive dyskinesia');
+INSERT INTO adverse_reactions (severity, reaction) VALUES('severe', 'laryngospasm');
 
 --AR Meds
-INSERT INTO ar_meds (ar_meds_id, reaction_id, med_id) VALUES();
+INSERT INTO ar_meds (reaction_id, med_id) VALUES();
 
 --Generics
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(300, 200, 'haloperidol');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(301, 201, 'bismuth subcitrate potassium');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(302, 201, 'metronidazole');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(303, 201, 'tetracycline hydrochloride');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(304, 202, 'alendronate sodium');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(305, 203, 'gabapentin');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(306, 204, 'baclofen');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(307, 205, 'oxybutynin chloride');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(308, 206, 'warfarin sodium');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(308, 207, 'citalopram hydrobromide');
-INSERT INTO generics (generic_id, med_id, generic_name) VALUES(308, 208, 'pantoprazole sodium');
+INSERT INTO generics (generic_name) VALUES('haloperidol');
+INSERT INTO generics (generic_name) VALUES('bismuth subcitrate potassium');
+INSERT INTO generics (generic_name) VALUES('metronidazole');
+INSERT INTO generics (generic_name) VALUES('tetracycline hydrochloride');
+INSERT INTO generics (generic_name) VALUES('alendronate sodium');
+INSERT INTO generics (generic_name) VALUES('gabapentin');
+INSERT INTO generics (generic_name) VALUES('baclofen');
+INSERT INTO generics (generic_name) VALUES('oxybutynin chloride');
+INSERT INTO generics (generic_name) VALUES('warfarin sodium');
+INSERT INTO generics (generic_name) VALUES('citalopram hydrobromide');
+INSERT INTO generics (generic_name) VALUES('pantoprazole sodium');
 
 --Medications
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(200, 'Haldol', true);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(201, 'Pylera', true);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(202, 'Fosamax', true);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(203, 'Neurontin', true);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(204, 'Gablofen', true);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(205, 'Ditropan XL', true);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(206, 'Coumadin', false);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(207, 'Celexa', false);
-INSERT INTO medications (med_id, brand_name, has_reaction) VALUES(208, 'Protonix', false);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Haldol', true);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Pylera', true);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Fosamax', true);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Neurontin', true);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Gablofen', true);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Ditropan XL', true);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Coumadin', false);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Celexa', false);
+INSERT INTO medications (brand_name, has_reaction) VALUES('Protonix', false);
 
 --Users
-INSERT INTO users (user_id, pseudo_id, pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('ClaireRF', 50, '01/01/21', '02/02/21', 'I69.391', 'R13.12');
-INSERT INTO users (user_id, pseudo_id, pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('JamesMF', 47, '03/03/21', '04/04/21', 'G20', 'R13.12');
-INSERT INTO users (user_id, pseudo_id, pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('BriannaFW', 31, '05/05/21', '06/06/21', 'G31.83', 'R13.11');
-INSERT INTO users (user_id, pseudo_id, pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('Roger@', 39, '07/07/21', '08/08/21', 'K20.0', 'R13.14');
+INSERT INTO users (pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('ClaireRF', 50, '01/01/21', '02/02/21', 'I69.391', 'R13.12');
+INSERT INTO users (pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('JamesMF', 47, '03/03/21', '04/04/21', 'G20', 'R13.12');
+INSERT INTO users (pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('BriannaFW', 31, '05/05/21', '06/06/21', 'G31.83', 'R13.11');
+INSERT INTO users (pseudo_name, age, date_of_eval, date_of_discharge, med_dx_icd, tx_dx_icd) VALUES('Roger@', 39, '07/07/21', '08/08/21', 'K20.0', 'R13.14');
 
 --User Meds
-INSERT INTO user_meds (user_med_id, med_id, user_id);
+INSERT INTO user_meds (med_id, user_id);
